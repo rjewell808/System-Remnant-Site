3ede0240a02ce7b8fe85bcd75c52d9a2019dca76
LITTLE-BERTHA <robertjewell808@gmail.com>
2018-07-16 23:43:23 -0400

    lots of changes
    - users
    - admin nav
    - content partials
    - articles model update
    - other pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
 
   # GET /articles
   # GET /articles.json
@@ -70,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :desc, :content, :image, :publish_date, :media_folder)
+      params.require(:article).permit(:title, :desc, :content, :image, :publish_date, :media_folder, :isShowing)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/media', to: 'static_pages#media'
+  get '/demo', to: 'static_pages#demo'
+  get '/demo-article', to: 'static_pages#demo-article'
 end
